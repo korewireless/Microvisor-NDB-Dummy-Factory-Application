b8289850025fecf6906fa249b29d4ba34b54e60f
Anton Gerasimov <agerasimov@twilio.com>
2023-05-15 17:12:11 +0200

    Get rid of cmsis_os2
    Seems to be a pretty transparent extra layer on top of FreeRTOS, let's
    stick to FreeRTOS API

--- HardwareTest/Src/main.c
+++ HardwareTest/Src/main.c
@@ -9,7 +9,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-#include "cmsis_os.h"
+#include "FreeRTOS.h"
 #include "mv_syscalls.h"
 
 #include "messages.h"
@@ -17,29 +17,6 @@
 #include "network.h"
 #include "test_runner.h"
 #include "system.h"
-
-osThreadId_t HardwareTestTask;
-const osThreadAttr_t HardwareTestTask_attributes = {
-    .name = "HardwareTestTask",
-    .priority = (osPriority_t)osPriorityNormal,
-    .stack_size = configMINIMAL_STACK_SIZE
-};
-
-osThreadId_t NetworkTask;
-const osThreadAttr_t NetworkTask_attributes = {
-    .name = "NetworkTask",
-    .priority = (osPriority_t)osPriorityNormal,
-    .stack_size = configMINIMAL_STACK_SIZE
-};
-
-osThreadId_t TestRunnerTask;
-const osThreadAttr_t TestRunnerTask_attributes = {
-    .name = "TestRunnerTask",
-    .priority = (osPriority_t)osPriorityNormal,
-    .stack_size = configMINIMAL_STACK_SIZE
-};
-
-void        SystemClock_Config(void);
 
 static struct NetworkTaskArgument network_task_argument;
 static struct HardwareTestTaskArgument hardware_test_task_argument;
@@ -49,9 +26,6 @@ int main(void) {
     /* Initialize the system */
     system_init();
 
-    /* Init scheduler */
-    osKernelInitialize();
-
     network_task_argument.in_queue = xQueueCreate(5, sizeof(enum Message));
     network_task_argument.out_queue = xQueueCreate(5, sizeof(enum Message));
 
@@ -59,11 +33,37 @@ int main(void) {
     hardware_test_task_argument.out_result_queue = xQueueCreate(5, sizeof(struct HardwareTestResultMessage));
 
     /* Create the thread(s) */
-    HardwareTestTask = osThreadNew(start_hardware_test_task, &hardware_test_task_argument, &HardwareTestTask_attributes);
-    NetworkTask = osThreadNew(start_network_task, &network_task_argument, &NetworkTask_attributes);
-    TestRunnerTask = osThreadNew(start_test_runner_task, &test_runner_task_argument, &TestRunnerTask_attributes);
+    if (!xTaskCreate(start_hardware_test_task,
+                     "HardwareTestTask",
+                     configMINIMAL_STACK_SIZE/sizeof(StackType_t),
+                     &hardware_test_task_argument,
+                     0,
+                     NULL))
+    {
+        mvTestingComplete(InternalErrorTestResult);
+    }
 
-    osKernelStart();
+    if (!xTaskCreate(start_network_task,
+                     "NetworkTask",
+                     configMINIMAL_STACK_SIZE/sizeof(StackType_t),
+                     &network_task_argument,
+                     0,
+                     NULL))
+    {
+        mvTestingComplete(InternalErrorTestResult);
+    }
+
+    if (!xTaskCreate(start_test_runner_task,
+                     "TestRunnerTask",
+                     configMINIMAL_STACK_SIZE/sizeof(StackType_t),
+                     &test_runner_task_argument,
+                     0,
+                     NULL))
+    {
+        mvTestingComplete(InternalErrorTestResult);
+    }
+
+    vTaskStartScheduler();
 
     while (1) {}
 }

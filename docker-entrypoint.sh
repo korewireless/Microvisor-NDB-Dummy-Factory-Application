350ad49207ed4ebae112c1caa146473407d52e64
Anton Gerasimov <agerasimov@twilio.com>
2023-05-17 12:51:58 +0200

    Fix docker build
    - Fix a typo in a path
    - Recreate artifacts directory on every build
    - Don't touch CMakeLists.txt

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,19 +4,20 @@ set -e
 cd $(dirname $0)
 
 [ -d build ] && rm -rf build
-[ -d artifacts ] && rm -rf artifacts
 
-echo ${MV_GA}
+mkdir build
 
-if [[ -n "${MV_GA}" ]]; then
-  # Build only
-  twilio microvisor:deploy . -b
-else 
-  # Build and deploy -- requires env vars for device SID and Twilio creds to be set
-  cd build
-  cmake ..
-  make -j$(nproc)
+cd build
+cmake ..
+make -j$(nproc)
 
-  twilio microvisor apps bundle ./HardwareTest/hardware_test_sample.bin ./HarwareTest/hardware_test_sample.bundle
+twilio microvisor apps bundle ./HardwareTest/hardware_test_sample.bin ./HardwareTest/hardware_test_sample.bundle
+
+if [[ -z "${MV_GA}" ]]; then
+  [ -d artifacts ] && rm -rf artifacts
+
+  mkdir artifacts
+
+  # Deploy -- requires env vars for device SID and Twilio creds to be set
   twilio microvisor apps create ./HardwareTest/hardware_test_sample.bundle --bundle-out ../artifacts/apptest.bundle
 fi

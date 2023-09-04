185e2f7dec4f0e2d72860415a2aadd04fcfcb023
Anton Gerasimov <agerasimov@twilio.com>
2023-09-04 17:14:48 +0200

    Rename project to ndb_hardware_test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ cd build
 cmake ..
 make -j$(nproc)
 
-twilio microvisor apps bundle ./HardwareTest/hardware_test_sample.bin ./HardwareTest/hardware_test_sample.bundle
+twilio microvisor apps bundle ./HardwareTest/ndb_hardware_test.bin ./HardwareTest/ndb_hardware_test.zip
 
 if [[ -z "${MV_GA}" ]]; then
   [ -d artifacts ] && rm -rf artifacts
@@ -19,5 +19,5 @@ if [[ -z "${MV_GA}" ]]; then
   mkdir artifacts
 
   # Deploy -- requires env vars for device SID and Twilio creds to be set
-  twilio microvisor apps create ./HardwareTest/hardware_test_sample.bundle --bundle-out ../artifacts/apptest.bundle
+  twilio microvisor apps create ./HardwareTest/ndb_hardware_test.zip --bundle-out ../artifacts/ndb_hardware_test.signed.zip
 fi
